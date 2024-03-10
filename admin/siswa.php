019fcc8d58ac66cf968bb4d8224304cac5499afd
riza <146099417+Faturz@users.noreply.github.com>
2024-03-10 10:32:06 +0700

    Add files via upload

---
+++ admin/siswa.php
@@ -0,0 +1,13 @@
+
+
+
+<?php
+
+include("index.php");
+?>
+<div class="container">
+<div class="row-1">
+    <h1>siswa</h1>
+</div>
+
+</div>
---
+++ main.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.menuActive')
+const nav = document.querySelector('.header')
+
+    menu.addEventListener('click', () => {
+
+    })
